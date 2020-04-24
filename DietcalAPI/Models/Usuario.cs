c6ad1f4e7f7a99c9c5a62edb4f18c5ce5a79cef2
Alvaro Martinez <armpmartinez@gmail.com>
2020-04-24 10:14:50 -0400

    agregando modelo de usuario

---
+++ DietcalAPI/Models/Usuario.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DietcalAPI.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int UsuarioId { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Contraseña { get; set; }
+        public string NombreUsuario { get; set; }
+        public string Email { get; set; }
+    }
+}
