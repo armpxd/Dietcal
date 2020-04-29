ba57d2e91c0919184fc04da25fe61a424c133c12
Alvaro Martinez <armpmartinez@gmail.com>
2020-04-28 23:19:16 -0400

    add sent email functionality

---
+++ DietcalAPI/Services/EmailService.cs
@@ -0,0 +1,38 @@
+using System.Net.Mail;
+using DietcalAPI.Models;
+using Microsoft.Extensions.Configuration;
+using System.Net;
+using System;
+using DietcalAPI.Utilities;
+
+namespace DietcalAPI.Services
+{
+    public class EmailService
+    {
+        private IConfiguration _configuration;
+
+        public EmailService (IConfiguration Iconfig)
+        {
+            _configuration = Iconfig;
+        }
+        public void RecoverPassword (string to)
+        {
+            using (MailMessage message = new MailMessage (_configuration.GetSection("MailSettings").GetSection("FromMail").Value,to))
+            {
+                message.Subject = "Usted ha solicitado un cambio de contraseña";
+                message.Body = GeneratePassword.newpassword();
+                message.IsBodyHtml = false;
+
+                SmtpClient smtp = new SmtpClient ();
+                smtp.Host = _configuration.GetValue<string>("MailSettings:Host");
+                smtp.Port = int.Parse(_configuration.GetSection("MailSettings").GetSection("Port").Value);
+                smtp.EnableSsl = true;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new System.Net.NetworkCredential(_configuration.GetSection("MailSettings").GetSection("Username").Value,
+                                                                    _configuration.GetSection("MailSettings").GetSection("Password").Value);
+                smtp.Send(message);
+                System.Threading.Thread.Sleep(3000);
+            }
+        }
+    }
+}
