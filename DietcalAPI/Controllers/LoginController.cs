e6f3cb3cffaa26cf6279b83ab414cd557bfec180
Alvaro Martinez <armpmartinez@gmail.com>
2020-05-01 17:34:53 -0400

    add forgetpassword method

--- DietcalAPI/Controllers/LoginController.cs
+++ DietcalAPI/Controllers/LoginController.cs
@@ -1,20 +1,24 @@
 using System.Linq;
 using DietcalAPI.Data;
 using DietcalAPI.Models;
+using DietcalAPI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace DietcalAPI.Controllers
 {
+    [Route("api/[controller]/[action]")]
     [ApiController]
-    [Route("api/[controller]")]
     public class LoginController : ControllerBase
     {
         private readonly ContextDietcal _dbDietcal;
-
-        public LoginController (ContextDietcal data)
+        private IConfiguration _configuration;
+        public LoginController (ContextDietcal data, IConfiguration Iconfig)
         {
             _dbDietcal = data;
+            _configuration = Iconfig;
         }
+        
         [HttpPost]
         public ActionResult Register([FromBody] Usuario user)
         {
@@ -22,13 +26,32 @@ namespace DietcalAPI.Controllers
 
             if (target == null)
             {
-                _dbDietcal.Add(user);
+                user.TemporyPassword = false;
+                _dbDietcal.Usuario.Add(user);
                 _dbDietcal.SaveChanges();
             }
             else
             {
                 return BadRequest("El usuario existe");
             }
+            return Ok();
+        }
+
+        [HttpPost]
+        public ActionResult ForgetPassword([FromForm]string email)
+        {
+            var target = _dbDietcal.Usuario.FirstOrDefault( x => x.Email == email);
+
+            if (target == null)
+            {
+                return NotFound();
+            }
+            EmailService newpassword = new EmailService(_configuration);
+            var password = newpassword.RecoverPassword(email);
+
+            target.Contraseña = password;
+            target.TemporyPassword = true;
+            _dbDietcal.SaveChanges();
             return Ok();
         }
         
