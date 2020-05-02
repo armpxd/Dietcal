87b6bb71adbdaf4baf45ffff6c0cc97949fccc57
Julio Antonio Ortiz Pol <julioantonioortizpol@Julios-MacBook-Pro.local>
2020-05-02 19:32:33 -0400

    created form without logic in login

--- diet_cal_mobile/lib/login.dart
+++ diet_cal_mobile/lib/login.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +32,79 @@ class Login extends StatelessWidget {
                 ),
               ),
               Container(
-                child: TextField(),
+                child: Form(
+                  key: _formKey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            bottom: 30, top: 40, left: 10, right: 10),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                              icon: Icon(Icons.person),
+                              hintText: 'Coloque su nombre de usuario',
+                              labelText: 'Usuario',
+                              border: OutlineInputBorder()),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter some text';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            bottom: 30, left: 10, right: 10),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                              icon: Icon(Icons.lock),
+                              hintText: 'Coloque su nombre de usuario',
+                              labelText: 'Usuario',
+                              border: OutlineInputBorder()),
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please enter some text';
+                            }
+                            return null;
+                          },
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Text("Recordar Contraseña"),
+                          Switch(
+                            value: true,
+                            onChanged: (bool valueChange) {},
+                          ),
+                        ],
+                      ),
+                      Center(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 16.0),
+                          child: RaisedButton(
+                            onPressed: () {
+                              // Validate will return true if the form is valid, or false if
+                              // the form is invalid.
+                              if (_formKey.currentState.validate()) {
+                                // Process data.
+                              }
+                            },
+                            child: Text('Iniciar Sección'),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text("¿Olvidaste tu contraseña?"),
+                ),
               )
             ],
           ),
