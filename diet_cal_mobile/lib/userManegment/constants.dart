1c7d23fc164ae6e6bc68ae560ecf6befb6ae0fb6
Julio Antonio Ortiz Pol <julioantonioortizpol@Julios-MacBook-Pro.local>
2020-05-03 12:34:29 -0400

    add all constants of literals for login

--- diet_cal_mobile/lib/userManegment/constants.dart
+++ diet_cal_mobile/lib/userManegment/constants.dart
@@ -2,12 +2,16 @@ import 'package:flutter/material.dart';
 
 class LoginLiterals {
   static const String userInputHintText = 'Coloque su nombre de usuario';
-  static const String userInputLabel = 'Coloque su nombre de usuario';
-  static const String userInputValidator = 'Coloque su nombre de usuario';
+  static const String userInputLabel = 'Usuario';
   static const Icon userInputIcon = Icon(Icons.person);
 
   static const String passwordInputHintText = 'Coloque su nombre de usuario';
-  static const String passwordInputLabel = 'Coloque su nombre de usuario';
-  static const String passwordInputValidator = 'Coloque su nombre de usuario';
+  static const String passwordInputLabel = 'Contraseña';
   static const Icon passwordInputIcon = Icon(Icons.lock);
+
+  static const String passwordRememberMe = "Recordar contraseña";
+
+  static const String login = "Iniciar Sección";
+
+  static const String forgotPassword = "¿Olvidaste tu contraseña?";
 }
