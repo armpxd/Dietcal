683ee1432d743b33e2d0a76af4c8ebb5ff5d37f1
Julio Antonio Ortiz Pol <julioantonioortizpol@Julios-MacBook-Pro.local>
2020-05-10 19:49:56 -0400

    add appName constant

--- diet_cal_mobile/lib/appConstants.dart
+++ diet_cal_mobile/lib/appConstants.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+const String appName = "DietCall";
 const Color primaryColor = Color(0xff51E084);
 const Color mainColorStrong = Color(0xff5ACD87);
 const Color mainColorLight = Color(0xff68EB9D);
