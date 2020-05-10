366792e0a5611c1c816eb0ac58f91117ee1a6151
Julio Antonio Ortiz Pol <julioantonioortizpol@Julios-MacBook-Pro.local>
2020-05-10 19:50:34 -0400

    add app routes

--- diet_cal_mobile/lib/main.dart
+++ diet_cal_mobile/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'userManegment/login.dart';
 import 'appConstants.dart';
+import 'userManegment/forgotPassword.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,20 +10,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primaryColor: primaryColor,
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-      ),
-      home: Login(),
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primaryColor: primaryColor,
+        ),
+        home: Login(),
+        routes: {
+          ForgotPassword.id: (context) => ForgotPassword(),
+        });
   }
 }
