a0120b88ef408afd3434a4ba8422ccd201e4a22f
Julio Antonio Ortiz Pol <julioantonioortizpol@Julios-MacBook-Pro.local>
2020-05-09 21:21:27 -0400

    add mainColor constants

--- diet_cal_mobile/lib/appConstants.dart
+++ diet_cal_mobile/lib/appConstants.dart
@@ -1,3 +1,6 @@
 import 'package:flutter/material.dart';
 
-Color primaryColor = Color(0xff51E084);
+const Color primaryColor = Color(0xff51E084);
+const Color mainColorStrong = Color(0xff5ACD87);
+const Color mainColorLight = Color(0xff68EB9D);
+//Color(0xff5ACD87)
